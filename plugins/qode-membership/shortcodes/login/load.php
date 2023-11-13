b4c1c23514b94c0c9ab3a54baafc7423b33f6bdb
David Johnson <david@bared.com.au>
2023-11-13 20:52:41 +1100

    Remove unnecessary plugins

--- plugins/qode-membership/shortcodes/login/load.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once QODE_MEMBERSHIP_SHORTCODES_PATH.'/login/functions.php';
-include_once QODE_MEMBERSHIP_SHORTCODES_PATH.'/login/login.php';
--- plugins/qode-membership/shortcodes/register/templates/register-message.php
+++
@@ -1,3 +0,0 @@
-<div class="qode-register-notice">
-	<h5 class="qode-register-notice-title"><?php echo esc_html($message); ?></h5>
-</div>
