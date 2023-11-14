3286deed0cf76216138a8d3adea4845b40248ded
David Johnson <david@bared.com.au>
2023-11-14 20:20:19 +1100

    chore: Move CSS styles to functions.php

--- themes/bridge-child/functions.php
+++ themes/bridge-child/functions.php
@@ -2,9 +2,13 @@
 
 if(!function_exists('bridge_qode_child_theme_enqueue_scripts')) {
 
-	Function bridge_qode_child_theme_enqueue_scripts() {
-		wp_register_style('bridge-childstyle', get_stylesheet_directory_uri() . '/style.css');
-		wp_enqueue_style('bridge-childstyle');
+	function bridge_qode_child_theme_enqueue_scripts() {
+		wp_register_style('bridge-child', get_stylesheet_directory_uri() . '/style.css');
+		wp_register_style('bridge-child-header', get_stylesheet_directory_uri() . '/header.css', '1.0');
+		wp_register_style('bridge-child-buttons', get_stylesheet_directory_uri() . '/buttons.css', '1.0');
+		wp_enqueue_style('bridge-child');
+		wp_enqueue_style('bridge-child-header');
+		wp_enqueue_style('bridge-child-buttons');
 	}
 
 	add_action('wp_enqueue_scripts', 'bridge_qode_child_theme_enqueue_scripts', 11);
